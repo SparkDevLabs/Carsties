9f7f024e9b15ad5b177b78555bbf99d772f67bc4
Shane Burke <sburke6@gmail.com>
2023-11-04 11:34:17 -0400

    end of section 2

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,117 @@
+﻿using System.Net.WebSockets;
+using AuctionService.Data;
+using AuctionService.Dto;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+     {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(a => a.Item)
+            .OrderBy(a => a.Item.Make)
+            .ToListAsync();
+
+            return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(a => a.Item)
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+        //Todo: add current user as the seller
+        auction.Seller = "test";
+
+        _context.Auctions.Add(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Cound not save the auction.");
+        }
+
+        return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<AuctionDto>> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions.FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        //todo: check that the seller name = current user
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+
+        //_mapper.Map(updateAuctionDto, auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Cound not update the auction.");
+        }
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<AuctionDto>> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+        _context.Auctions.Remove(auction);
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result)
+        {
+            return BadRequest("Cound not delete the auction.");
+        }
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+}
---
+++ src/AuctionService/Profiles/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.Dto;
+using AutoMapper;
+
+namespace AuctionService.Profiles;
+
+public class MappingProfiles : Profile
+{
+  public MappingProfiles()
+  {
+    //map
+    CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+    CreateMap<Item, AuctionDto>();
+    //reverse map 
+    CreateMap<CreateAuctionDto, Auction>().ForMember(x => x.Item, o => o.MapFrom(s => s));
+    CreateMap<CreateAuctionDto, Item>();
+
+  }
+}
